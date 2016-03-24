600db4e5ed8517f7e11c6bce918334d50f1fcf0f
Warren Ferrell <warren.ferrell@colorado.edu>
2016-03-23 21:02:04 -0600

    Classes

---
+++ src/main/Group.java
@@ -0,0 +1,12 @@
+package main;
+
+import Entities.File;
+
+/**
+ * Created by Warren on 3/23/2016.
+ */
+public class Group {
+    File<String> name;
+    File<ID> members;
+    File<ID> calendar;
+}
