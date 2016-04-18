748d4ecfc01b1214c889910a28b8421dc4496fce
Warren Ferrell <warren.ferrell@colorado.edu>
2016-04-17 21:40:23 -0600

    Changed testing procedures again

--- src/Test/ExecutorTestDriver.java
+++ src/Test/ExecutorTestDriver.java
@@ -26,20 +26,35 @@ class ExecutorTestDriver {
         Database DB = Database.GetDB();
 
         int number_of_tests = 3;
-        int number_of_tasks = 1024;
         long averaged_duration, max, min, test_duration;
 
+        ArrayList<Integer> number_of_tasks = new ArrayList<>();
+        number_of_tasks.add( 1 );
+        number_of_tasks.add( 2 );
+        number_of_tasks.add( 4 );
+        number_of_tasks.add( 8 );
+        number_of_tasks.add( 16 );
+        number_of_tasks.add( 32 );
+        number_of_tasks.add( 128 );
+        number_of_tasks.add( 256 );
+        number_of_tasks.add( 512 );
+        number_of_tasks.add( 1024 );
+        number_of_tasks.add( 2048 );
+        number_of_tasks.add( 4096 );
+        //number_of_tasks.add( 8192 );
+        //number_of_tasks.add( 16384 );
+
         ArrayList<Integer> number_of_threads = new ArrayList<>();
-        number_of_threads.add( 1 );
-        number_of_threads.add( 2 );
-        number_of_threads.add( 4 );
-        number_of_threads.add( 8 );
-        number_of_threads.add( 16 );
-        number_of_threads.add( 32 );
+//        number_of_threads.add( 1 );
+//        number_of_threads.add( 2 );
+//        number_of_threads.add( 4 );
+//        number_of_threads.add( 8 );
+//        number_of_threads.add( 16 );
+//        number_of_threads.add( 32 );
         number_of_threads.add( 64 );
-        number_of_threads.add( 128 );
-        number_of_threads.add( 256 );
-        number_of_threads.add( 512 );
+//        number_of_threads.add( 128 );
+//        number_of_threads.add( 256 );
+//        number_of_threads.add( 512 );
 
 
         ArrayList<String> types_of_lists = new ArrayList<>();
@@ -56,74 +71,77 @@ class ExecutorTestDriver {
         /* Total memory currently available to the JVM */
         System.out.println("Total memory available to JVM (bytes): " + Runtime.getRuntime().totalMemory());
 
-        System.out.println( "Running " + number_of_tasks + " tasks, averaged on " + number_of_tests + " trials, throwing away max and min" );
+        System.out.println("Running " + number_of_tests + " trials, throwing away max and min");
 
-        System.out.println( "num_processors, number of tasks, list_type, pool size, average duration(ns)" );
-        for( String list_type : types_of_lists )
-        {
-            for( int nThreads : number_of_threads )
-            {
-                averaged_duration = 0; max = Long.MIN_VALUE; min = Long.MAX_VALUE;
-                for( int loop = 0; loop < number_of_tests; loop++ )
-                {
-                    DB.Initialize( path_database, list_type, true );
-                    long startTime = System.nanoTime();
+        System.out.println( "num_processors, number of tasks, list_type, pool size, average duration(ms)" );
 
-                    ExecutorService manager = Executors.newFixedThreadPool( nThreads );
+        for (int numTasks : number_of_tasks) {
+            for (String list_type : types_of_lists) {
+                for (int nThreads : number_of_threads) {
+                    averaged_duration = 0;
+                    max = Long.MIN_VALUE;
+                    min = Long.MAX_VALUE;
+                    for (int loop = 0; loop < number_of_tests; loop++) {
+                        DB.Initialize(path_database, list_type, true);
+                        long startTime = System.nanoTime();
 
-                    // do all the first creations
-                    for( int i = 0; i < number_of_tasks; ++i ) {
-                        manager.submit(new dbResolverThrd(i, dbThrd.Op.CREATE, dbThrd.Col.CALENDAR, Arrays.asList("Ca" + i, "justin")));
-                    }
+                        ExecutorService manager = Executors.newFixedThreadPool(nThreads);
 
-                    manager.shutdown();
-                    try {
-                        manager.awaitTermination(3, TimeUnit.SECONDS);
-                    }
-                    catch( InterruptedException e ) {
-                        manager.shutdownNow();
-                        Thread.currentThread().interrupt();
-                    }
+                        // do all the first creations
+                        for (int i = 0; i < numTasks; ++i) {
+                            manager.submit(new dbResolverThrd(i, dbThrd.Op.CREATE, dbThrd.Col.CALENDAR, Arrays.asList("Ca" + i, "justin")));
+                        }
 
-                    // do a bunch of edits
-                    manager = Executors.newFixedThreadPool( nThreads );
-                    for( int i = 0; i < number_of_tasks; ++i ) {
+                        manager.shutdown();
+                        try {
+                            manager.awaitTermination(3, TimeUnit.SECONDS);
+                        } catch (InterruptedException e) {
+                            manager.shutdownNow();
+                            Thread.currentThread().interrupt();
+                        }
 
-                        manager.submit(new dbResolverThrd(i, dbThrd.Op.EDIT, dbThrd.Col.CALENDAR, new Integer(i), Arrays.asList(new MicroMap<>("title", "TEST"))));
-                    }
+                        // do a bunch of edits
+                        manager = Executors.newFixedThreadPool(nThreads);
+                        for (int i = 0; i < numTasks; ++i) {
 
-                    manager.shutdown();
-                    try {
-                        manager.awaitTermination(3, TimeUnit.SECONDS);
-                    }
-                    catch( InterruptedException e ) {
-                        manager.shutdownNow();
-                        Thread.currentThread().interrupt();
-                    }
+                            manager.submit(new dbResolverThrd(i, dbThrd.Op.EDIT, dbThrd.Col.CALENDAR, new Integer(i), Arrays.asList(new MicroMap<>("title", "TEST"))));
+                        }
 
-                    // do a bunch of deletions
-                    manager = Executors.newFixedThreadPool( nThreads );
-                    for( int i = 0; i < number_of_tasks; ++i ) {
-                        manager.submit(new dbResolverThrd(i, dbThrd.Op.DELETE, dbThrd.Col.CALENDAR, new Integer(i)));
-                    }
-                    manager.shutdown();
-                    try {
-                        manager.awaitTermination(3, TimeUnit.SECONDS);
-                    }
-                    catch( InterruptedException e ) {
-                        manager.shutdownNow();
-                        Thread.currentThread().interrupt();
-                    }
-                    long endTime = System.nanoTime();
-                    test_duration = (endTime - startTime);
-                    if (test_duration > max) { max = test_duration; }
-                    if (test_duration < min) { min = test_duration; }
-                    averaged_duration += test_duration;
-                } // end number_of_tests loop
-                averaged_duration = (averaged_duration - max - min) / (number_of_tests - 2);
-                System.out.println( num_processors + ", " + number_of_tasks + ", "+ list_type + ", " + nThreads + ", " +  averaged_duration );
-            } // end nThreads loop
-        } // end types of lists loop
+                        manager.shutdown();
+                        try {
+                            manager.awaitTermination(3, TimeUnit.SECONDS);
+                        } catch (InterruptedException e) {
+                            manager.shutdownNow();
+                            Thread.currentThread().interrupt();
+                        }
+
+                        // do a bunch of deletions
+                        manager = Executors.newFixedThreadPool(nThreads);
+                        for (int i = 0; i < numTasks; ++i) {
+                            manager.submit(new dbResolverThrd(i, dbThrd.Op.DELETE, dbThrd.Col.CALENDAR, new Integer(i)));
+                        }
+                        manager.shutdown();
+                        try {
+                            manager.awaitTermination(3, TimeUnit.SECONDS);
+                        } catch (InterruptedException e) {
+                            manager.shutdownNow();
+                            Thread.currentThread().interrupt();
+                        }
+                        long endTime = System.nanoTime();
+                        test_duration = (endTime - startTime);
+                        if (test_duration > max) {
+                            max = test_duration;
+                        }
+                        if (test_duration < min) {
+                            min = test_duration;
+                        }
+                        averaged_duration += test_duration;
+                    } // end number_of_tests loop
+                    averaged_duration = ( ( (averaged_duration - max - min) / (number_of_tests - 2) ) / 1000000 );
+                    System.out.println(num_processors + ", " + numTasks + ", " + list_type + ", " + nThreads + ", " + averaged_duration);
+                } // end nThreads loop
+            } // end types of lists loop
+        }
 
         
 
--- src/Test/ThreadSpawnTestDriver.java
+++ src/Test/ThreadSpawnTestDriver.java
@@ -27,28 +27,41 @@ class ThreadSpawnTestDriver {
         Database DB = Database.GetDB();
 
         int number_of_tests = 3;
-        int number_of_tasks = 1024;
         int i, k;
         long averaged_duration, max, min, test_duration;
 
+        ArrayList<Integer> number_of_tasks = new ArrayList<>();
+        number_of_tasks.add(1);
+        number_of_tasks.add(2);
+        number_of_tasks.add(4);
+        number_of_tasks.add(8);
+        number_of_tasks.add(16);
+        number_of_tasks.add(32);
+        number_of_tasks.add(128);
+        number_of_tasks.add(256);
+        number_of_tasks.add(512);
+        number_of_tasks.add(1024);
+        number_of_tasks.add(2048);
+        number_of_tasks.add(4096);
+        //number_of_tasks.add( 1 );
 
         ArrayList<Integer> thread_Limit = new ArrayList<>();
-        thread_Limit.add( 1 );
-        thread_Limit.add( 2 );
-        thread_Limit.add( 4 );
-        thread_Limit.add( 8 );
-        thread_Limit.add( 16 );
-        thread_Limit.add( 32 );
-        thread_Limit.add( 64 );
-        thread_Limit.add( 128 );
-        thread_Limit.add( 256 );
-        thread_Limit.add( 512 );
+//        thread_Limit.add(1);
+//        thread_Limit.add(2);
+//        thread_Limit.add(4);
+//        thread_Limit.add(8);
+//        thread_Limit.add(16);
+//        thread_Limit.add(32);
+        thread_Limit.add(64);
+//        thread_Limit.add(128);
+//        thread_Limit.add(256);
+//        thread_Limit.add(512);
 
         ArrayList<String> types_of_lists = new ArrayList<>();
         types_of_lists.add("Lazy");
         types_of_lists.add("LockFree");
 
-        ArrayList<dbResolverThrd> threads = zeroedList(number_of_tasks);
+        ArrayList<dbResolverThrd> threads = zeroedList(number_of_tasks.get(number_of_tasks.size() - 1));
 
         /* Total number of processors or cores available to the JVM */
         int num_processors = Runtime.getRuntime().availableProcessors();
@@ -60,56 +73,65 @@ class ThreadSpawnTestDriver {
         /* Total memory currently available to the JVM */
         System.out.println("Total memory available to JVM (bytes): " + Runtime.getRuntime().totalMemory());
 
-        System.out.println( "Running " + number_of_tasks + " tasks, averaged on " + number_of_tests + " trials, throwing away max and min" );
+        System.out.println("Running " + number_of_tests + " trials, throwing away max and min");
 
-        System.out.println( "num_processors, number of tasks, list_type, thread limit, average duration(ns)" );
-        for (String list_type : types_of_lists) {
-            for (int threadLimit : thread_Limit) {
-                averaged_duration = 0; max = Long.MIN_VALUE; min = Long.MAX_VALUE;
-                for (int loop = 0; loop < number_of_tests; loop++) {
-                    DB.Initialize(path_database, list_type, true);
-                    long startTime = System.nanoTime();
+        System.out.println("num_processors, number of tasks, list_type, thread limit, average duration(ms)");
+        for (int numTasks : number_of_tasks) {
+            for (String list_type : types_of_lists) {
+                for (int threadLimit : thread_Limit) {
+                    averaged_duration = 0;
+                    max = Long.MIN_VALUE;
+                    min = Long.MAX_VALUE;
+                    for (int loop = 0; loop < number_of_tests; loop++) {
+                        DB.Initialize(path_database, list_type, true);
+                        long startTime = System.nanoTime();
 
-                    k = 0; // do all the first creations
-                    while (k < number_of_tasks) {
-                        for (i = 0; i < threadLimit && (k < number_of_tasks); ++i) {
-                            threads.set(i,new dbResolverThrd(i, dbThrd.Op.CREATE, dbThrd.Col.CALENDAR, Arrays.asList("Ca" + k++, "justin")));
-                            threads.get(i).start();
+                        k = 0; // do all the first creations
+                        while (k < numTasks) {
+                            for (i = 0; i < threadLimit && (k < numTasks); ++i) {
+                                threads.set(i, new dbResolverThrd(i, dbThrd.Op.CREATE, dbThrd.Col.CALENDAR, Arrays.asList("Ca" + k++, "justin")));
+                                threads.get(i).start();
+                            }
+                            threadsJoin(threads, threadLimit, numTasks);
                         }
-                        threadsJoin(threads, threadLimit);
-                    }
 
-                    k = 0; // do a bunch of edits
-                    while (k < number_of_tasks) {
-                        for (i = 0; i < threadLimit && (k < number_of_tasks); ++i) {
-                            threads.set(i,new dbResolverThrd(i, dbThrd.Op.EDIT, dbThrd.Col.CALENDAR, k++, Arrays.asList(new MicroMap<>("title", "TEST"))));
-                            threads.get(i).start();
+                        k = 0; // do a bunch of edits
+                        while (k < numTasks) {
+                            for (i = 0; i < threadLimit && (k < numTasks); ++i) {
+                                threads.set(i, new dbResolverThrd(i, dbThrd.Op.EDIT, dbThrd.Col.CALENDAR, k++, Arrays.asList(new MicroMap<>("title", "TEST"))));
+                                threads.get(i).start();
+                            }
+                            threadsJoin(threads, threadLimit, numTasks);
                         }
-                        threadsJoin(threads, threadLimit);
-                    }
 
-                    k = 0; // do a bunch of deletions
-                    while (k < number_of_tasks) {
-                        for (i = 0; i < threadLimit && (k < number_of_tasks); ++i) {
-                            threads.set(i,new dbResolverThrd(i, dbThrd.Op.DELETE, dbThrd.Col.CALENDAR, k++));
-                            threads.get(i).start();
+                        k = 0; // do a bunch of deletions
+                        while (k < numTasks) {
+                            for (i = 0; i < threadLimit && (k < numTasks); ++i) {
+                                threads.set(i, new dbResolverThrd(i, dbThrd.Op.DELETE, dbThrd.Col.CALENDAR, k++));
+                                threads.get(i).start();
+                            }
+                            threadsJoin(threads, threadLimit, numTasks);
                         }
-                        threadsJoin(threads, threadLimit);
-                    }
-                    long endTime = System.nanoTime();
-                    test_duration = (endTime - startTime);
-                    if (test_duration > max) { max = test_duration; }
-                    if (test_duration < min) { min = test_duration; }
-                    averaged_duration += test_duration;
-                } // end number_of_tests loop
-                averaged_duration = (averaged_duration - max - min) / (number_of_tests - 2);
-                System.out.println( num_processors + ", " + number_of_tasks + ", " + list_type + ", " + threadLimit + ", " + averaged_duration );
-            } // end nThreads loop
-        } // end types of lists loop
+                        long endTime = System.nanoTime();
+                        test_duration = (endTime - startTime);
+                        if (test_duration > max) {
+                            max = test_duration;
+                        }
+                        if (test_duration < min) {
+                            min = test_duration;
+                        }
+                        averaged_duration += test_duration;
+                    } // end number_of_tests loop
+                    averaged_duration = ( ( (averaged_duration - max - min) / (number_of_tests - 2) ) / 1000000 );
+                    System.out.println(num_processors + ", " + numTasks + ", " + list_type + ", " + threadLimit + ", " + averaged_duration);
+                } // end nThreads loop
+            } // end types of lists loop
+        } //end number of tasks loop
+
     }
 
-    static void threadsJoin(ArrayList<dbResolverThrd> threads, int nThreads) {
-        for (int j = 0; j < nThreads && j < threads.size(); ++j) {
+    static void threadsJoin(ArrayList<dbResolverThrd> threads, int nThreads, int numTasks) {
+        for (int j = 0; (j < nThreads) && (j < numTasks); ++j) {
             try {
                 threads.get(j).join();
             } catch (InterruptedException e) {
